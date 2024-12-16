099167dbe242465e01dcd801aaf2aeb25dae02d5
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-12-16 14:58:11 -0500

    adds responsive boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <section className="m-12 flex h-screen items-center justify-center bg-gray-400 text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-20">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi,
+          architecto. Incidunt ad excepturi minima? Ullam dicta quasi placeat
+          quos at adipisci. Quidem voluptas ipsam minima corporis architecto
+          laudantium consectetur maiores!
+        </p>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
