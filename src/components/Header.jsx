ce61233ee27847d0aa4d80ab16e740d92def5e30
jdauria97 <josephdauria97@gmail.com>
2025-03-14 20:51:59 -0400

    styles menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
   return (
     <div
       className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
-        openNavigation ? "ng-n-8" : "bg-n-8/90 backdrop-blur-sm"
+        openNavigation ? "bg-n-8/90" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
